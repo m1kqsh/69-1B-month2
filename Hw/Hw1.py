08b61da8abb8766b815afcaca64a98eb785de003
Imran <muratbekovimran6@gmail.com>
2026-08-18 01:15:59 +0600

    ÐдÑз1Ð

--- Hw/Hw1.py
+++ Hw/Hw1.py
@@ -10,5 +10,14 @@ class Person:
 
 
 Man_1 = Person(name="Nurlan",birth_date="26.09.2010",occupation="программист",higher_education="нет")
+Man_2 = Person(name="Azamat",birth_date="15.04.2005",occupation="врач",higher_education="есть")
+Man_3 = Person(name="Aibek",birth_date="08.12.2008",occupation="дизайнер",higher_education="нет")
+Man_4 = Person(name="Bekzat",birth_date="21.07.2002",occupation="инженер",higher_education="есть")
+Man_5 = Person(name="Timur",birth_date="03.02.2012",occupation="ученик",higher_education="нет")
+
 
 print(Man_1)
+print(Man_2)
+print(Man_3)
+print(Man_4)
+print(Man_5)
